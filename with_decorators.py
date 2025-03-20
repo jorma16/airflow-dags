b2a587045043867ec3e91827884b8f8a76bd9b7f
Jorge Marchan <jmarchan@avantio.com>
2025-03-20 10:04:47 +0100

    feat: decorators

--- with_decorators.py
+++ with_decorators.py
@@ -7,3 +7,6 @@ from airflow.operators.empty import EmptyOperator
 @dag(start_date=datetime.datetime(2021, 1, 1), schedule=None)
 def generate_dag():
     EmptyOperator(task_id="task")
+
+
+generate_dag()
